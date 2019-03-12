4c34494c7bb68e3f0f7b043aaed9ab1d50b0c0fe
Andreyang <1019439642@qq.com>
2019-03-12 19:12:45 +0800

    zhonghuazidian

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(bodyParser.json())
 app.engine('html', require('express-art-template'))
 
 // 公开指定目录 直接通过 /a/xx 的方式访问 public 目录中的所有资源
-app.use('/a', express.static(path.join(__dirname, 'public')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/js', express.static(path.join(__dirname, 'public/js')))
 
 app.all("*",function(req, res, next){
     //设置允许跨域的域名，*代表允许任意域名跨域
@@ -19,7 +20,7 @@ app.all("*",function(req, res, next){
     res.header("Access-Control-Allow-Headers","content-type");
     //跨域允许的请求方式
     res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-    if (req.method.toLowerCase() == 'options')
+    if (req.method.toLowerCase() === 'options')
         res.send(200);  //让options尝试请求快速结束
     else
         next();
--- router.js
+++ router.js
@@ -1,15 +1,193 @@
 var express = require('express')
 var router = express.Router()
-
+var fs = require('fs')
 router.get('/', function (req, res, next) {
     res.render('index.html')
 })
 
-router.post('/postrequest', function (req, res, next) {
-  res.json({
-    hello: "你好!"
+router.get('/character', function (req, res, next) {
+  const id = req.query.id
+  const temp = req.query.temp
+  const page = req.query.page
+  const pageSize = req.query.pageSize
+  console.log(req.query)
+  fs.readFile('./data/word.json', 'utf-8', function (err, data) {
+    if(err) {
+      return next
+    }
+    const dataObj = JSON.parse(data)
+    const dataArr = []
+    switch (id) {
+      case '1':
+        for (let i = 0; i < dataObj.length; i++) {
+          if(dataObj[i].word === temp) {
+            dataArr.push(dataObj[i])
+          }
+        }
+        res.json({
+          status: 0,
+          data: dataArr.slice(pageSize*page-pageSize, pageSize*page),
+          total: dataArr.length
+        });break
+      case '2':
+        for (let i = 0; i < dataObj.length; i++) {
+          if(dataObj[i].oldword === temp) {
+            dataArr.push(dataObj[i])
+          }
+        }
+        res.json({
+          status: 0,
+          data: dataArr.slice(pageSize*page-pageSize, pageSize*page),
+          total: dataArr.length
+        });break
+      case '3':
+        for (let i = 0; i < dataObj.length; i++) {
+          if(dataObj[i].strokes === temp) {
+            dataArr.push(dataObj[i])
+          }
+        }
+        res.json({
+          status: 0,
+          data: dataArr.slice(pageSize*page-pageSize, pageSize*page),
+          total: dataArr.length
+        });break
+      case '4':
+        const arr = [
+          {'a': ['ā', 'á', 'ǎ', 'à']},
+          {'e': ['ē', 'é', 'ě', 'è']},
+          {'i': ['ī', 'í', 'ǐ', 'ì']},
+          {'o': ['ō', 'ó', 'ǒ', 'ò']},
+          {'u': ['ū', 'ú', 'ǔ', 'ù', 'ǘ', 'ǚ', 'ü', 'ǜ']}
+        ]
+        for (let i = 0; i < dataObj.length; i++) {
+          for (let j = 0; j < arr.length; j++) {
+            for(let k = 0; k < Object.values(arr[j])[0].length; k++) {
+              // console.log(dataObj[i].pinyin === "hánɡ", 'g' === 'ɡ')
+              if(dataObj[i].pinyin.includes(Object.values(arr[j])[0][k])) {
+                if(dataObj[i].pinyin.replace(Object.values(arr[j])[0][k], Object.keys(arr[j])[0]) === temp) {
+                  dataArr.push(dataObj[i])
+                }
+              }
+            }
+          }
+        }
+        res.json({
+          status: 0,
+          data: dataArr.slice(pageSize*page-pageSize, pageSize*page),
+          total: dataArr.length,
+        });break
+    }
   })
 })
 
+router.get('/words', function (req, res, next) {
+  const temp = req.query.temp
+  const page = req.query.page
+  const pageSize = req.query.pageSize
+  console.log(req.query)
+  fs.readFile('./data/ci.json', 'utf-8', function (err, data) {
+    if(err) {
+      return next
+    }
+    const dataObj = JSON.parse(data)
+    const dataArr = []
+    for (let i = 0; i < dataObj.length; i++) {
+      if(dataObj[i].ci.includes(temp)) {
+        dataArr.push(dataObj[i])
+      }
+    }
+    res.json({
+      status: 0,
+      data: dataArr.slice(pageSize*page-pageSize, pageSize*page),
+      total: dataArr.length,
+    })
+  })
+})
+
+router.get('/allegorical', function (req, res, next) {
+  const temp = req.query.temp
+  const page = req.query.page
+  const pageSize = req.query.pageSize
+  console.log(req.query)
+  fs.readFile('./data/xiehouyu.json', 'utf-8', function (err, data) {
+    if(err) {
+      return next
+    }
+    const dataObj = JSON.parse(data)
+    const dataArr = []
+    for (let i = 0; i < dataObj.length; i++) {
+      if(dataObj[i].riddle.includes(temp)) {
+        dataArr.push(dataObj[i])
+      }
+    }
+    res.json({
+      status: 0,
+      data: dataArr.slice(pageSize*page-pageSize, pageSize*page),
+      total: dataArr.length,
+    })
+  })
+})
+
+router.get('/idiom', function (req, res, next) {
+  const id = req.query.id
+  const temp = req.query.temp
+  const page = req.query.page
+  const pageSize = req.query.pageSize
+  console.log(req.query)
+  fs.readFile('./data/idiom.json', 'utf-8', function (err, data) {
+    if(err) {
+      return next
+    }
+    const dataObj = JSON.parse(data)
+    const dataArr = []
+    switch (id) {
+      case '1':
+        for (let i = 0; i < dataObj.length; i++) {
+          if(dataObj[i].word.includes(temp)) {
+            dataArr.push(dataObj[i])
+          }
+        }
+        res.json({
+          status: 0,
+          data: dataArr.slice(pageSize*page-pageSize, pageSize*page),
+          total: dataArr.length
+        });break
+      case '4':
+        const arr = [
+          {'a': ['ā', 'á', 'ǎ', 'à']},
+          {'e': ['ē', 'é', 'ě', 'è']},
+          {'i': ['ī', 'í', 'ǐ', 'ì']},
+          {'o': ['ō', 'ó', 'ǒ', 'ò']},
+          {'u': ['ū', 'ú', 'ǔ', 'ù', 'ǘ', 'ǚ', 'ü', 'ǜ']}
+        ]
+        let str = ''
+        for (let i = 0; i < dataObj.length; i++) {
+          const py = dataObj[i].pinyin.replace(/ /g, '')
+          for (let j of py ) {
+            for (let k = 0; k < arr.length; k++) {
+              for (let x = 0; x < Object.values(arr[k])[0].length; x++) {
+                if (j === Object.values(arr[k])[0][x]) {
+                  j = Object.keys(arr[k])[0]
+                }
+              }
+            }
+            str += j
+          }
+          if (str.includes(temp)) {
+            dataArr.push(dataObj[i])
+          }
+          str = ''
+          if (dataObj[i].abbreviation.includes(temp)) {
+            dataArr.push(dataObj[i])
+          }
+        }
+        res.json({
+          status: 0,
+          data: dataArr.slice(pageSize*page-pageSize, pageSize*page),
+          total: dataArr.length,
+        });break
+    }
+  })
+})
 
 module.exports = router
